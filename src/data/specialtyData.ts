28dfec9a68ac1d003adc48329500a88ce524fd22
RoseyCoUk <roseycomanagement@roseyco.co.uk>
2026-01-18 20:26:39 +0000

    feat(04-menu-01): add specialty menu data

---
+++ src/data/specialtyData.ts
@@ -0,0 +1,201 @@
+export interface SpecialtyItem {
+  image: string;
+  name: string;
+  desc?: string;
+}
+
+export interface BobaItem {
+  image: string;
+  name: string;
+  desc: string;
+}
+
+export const bingsuItems: SpecialtyItem[] = [
+  {
+    image: '/Bingsu/BerryliciousBingsu.webp',
+    name: 'Berrylicious',
+    desc: 'Mixed berries with sweet berry sauce',
+  },
+  {
+    image: '/Bingsu/BiscoffBananaBingsu.webp',
+    name: 'Biscoff Banana',
+    desc: 'Lotus Biscoff cookies with fresh banana',
+  },
+  {
+    image: '/Bingsu/CaramelAppleBingsu.webp',
+    name: 'Caramel Apple',
+    desc: 'Caramel sauce with crisp apple',
+  },
+  {
+    image: '/Bingsu/DubaiChocolateBingsu.webp',
+    name: 'Dubai Chocolate',
+    desc: 'Rich chocolate with premium toppings',
+  },
+  {
+    image: '/Bingsu/MangoCheesecakeBingsu.webp',
+    name: 'Mango Cheesecake',
+    desc: 'Fresh mango with cheesecake pieces',
+  },
+  {
+    image: '/Bingsu/MangoCoconutBingsu.webp',
+    name: 'Mango Coconut',
+    desc: 'Tropical mango with coconut flakes',
+  },
+  {
+    image: '/Bingsu/MatchaBingsu.webp',
+    name: 'Matcha',
+    desc: 'Premium matcha green tea powder',
+  },
+  {
+    image: '/Bingsu/OreoChocolateBingsu.webp',
+    name: 'Oreo Chocolate',
+    desc: 'Chocolate with crushed Oreo cookies',
+  },
+  {
+    image: '/Bingsu/OreoCrunchBingsu.webp',
+    name: 'Oreo Crunch',
+    desc: 'Oreo pieces with chocolate sauce',
+  },
+  {
+    image: '/Bingsu/StrawberryCheesecakeBingsu.webp',
+    name: 'Strawberry Cheesecake',
+    desc: 'Fresh strawberries with cheesecake',
+  },
+  {
+    image: '/Bingsu/TaroBingsu.webp',
+    name: 'Taro',
+    desc: 'Sweet taro root with purple color',
+  },
+  {
+    image: '/Bingsu/ThaiTeaBingsu.webp',
+    name: 'Thai Tea',
+    desc: 'Aromatic Thai tea with creamy toppings',
+  },
+];
+
+export const goldenToastItems: SpecialtyItem[] = [
+  {
+    image: '/GoldenToast/ChocoBananaGT.webp',
+    name: 'Choco Banana',
+    desc: 'Rich chocolate spread with fresh sliced bananas',
+  },
+  {
+    image: '/GoldenToast/MixedBerryGT.webp',
+    name: 'Mixed Berry',
+    desc: 'Fresh mixed berries with sweet berry sauce',
+  },
+  {
+    image: '/GoldenToast/ApplePieGT.webp',
+    name: 'Apple Pie',
+    desc: 'Warm apple pie filling with cinnamon and caramel',
+  },
+  {
+    image: '/GoldenToast/SmoreGT.webp',
+    name: "S'More",
+    desc: 'Chocolate, marshmallow, and graham cracker crumbles',
+  },
+  {
+    image: '/GoldenToast/MangoGT.webp',
+    name: 'Mango',
+    desc: 'Fresh mango slices with tropical mango sauce',
+  },
+];
+
+export const milkTeaItems: SpecialtyItem[] = [
+  {
+    image: '/MilkTea/TaroMilkTea.webp',
+    name: 'Taro',
+  },
+  {
+    image: '/MilkTea/AssamBlackMilkTea.webp',
+    name: 'Assam Black',
+  },
+  {
+    image: '/MilkTea/ThaiTeaMilkTea.webp',
+    name: 'Thai Tea',
+  },
+  {
+    image: '/MilkTea/DarkOolongMilkTea.webp',
+    name: 'Dark Oolong',
+  },
+  {
+    image: '/MilkTea/MatchaMilkTea.webp',
+    name: 'Matcha',
+  },
+  {
+    image: '/MilkTea/ChocolateMilkTea.webp',
+    name: 'Chocolate',
+  },
+  {
+    image: '/MilkTea/StrawberryMilkTea.webp',
+    name: 'Strawberry',
+  },
+  {
+    image: '/MilkTea/BrownSugarMilkTea.webp',
+    name: 'Brown Sugar',
+  },
+  {
+    image: '/MilkTea/BlueberryMilkTea.webp',
+    name: 'Blueberry',
+  },
+  {
+    image: '/MilkTea/CaramelMilkTea.webp',
+    name: 'Caramel',
+  },
+];
+
+export const fruitTeaItems: SpecialtyItem[] = [
+  {
+    image: '/FruitTea/LemonFruitTea.webp',
+    name: 'Lemon',
+    desc: 'Assam Black Tea',
+  },
+  {
+    image: '/FruitTea/RaspberryFruitTea.webp',
+    name: 'Raspberry',
+    desc: 'Jasmine Green Tea',
+  },
+  {
+    image: '/FruitTea/KumquatFruitTea.webp',
+    name: 'Kumquat',
+    desc: 'Jasmine Green Tea',
+  },
+  {
+    image: '/FruitTea/PassionfruitFruitTea.webp',
+    name: 'Passionfruit',
+    desc: 'Assam Black Tea',
+  },
+  {
+    image: '/FruitTea/MangoFruitTea.webp',
+    name: 'Mango',
+    desc: 'Jasmine Green Tea',
+  },
+  {
+    image: '/FruitTea/AppleFruitTea.webp',
+    name: 'Apple',
+    desc: 'Jasmine Green Tea',
+  },
+  {
+    image: '/FruitTea/LycheeFruitTea.webp',
+    name: 'Lychee',
+    desc: 'Jasmine Green Tea',
+  },
+];
+
+export const bobaItems: BobaItem[] = [
+  {
+    image: '/Boba/TapiocaBoba.webp',
+    name: 'Tapioca',
+    desc: 'Classic chewy black tapioca pearls',
+  },
+  {
+    image: '/Boba/PoppingBoba.webp',
+    name: 'Popping Boba',
+    desc: 'Juicy fruit-flavored popping pearls',
+  },
+  {
+    image: '/Boba/JellyBoba.webp',
+    name: 'Rainbow Jelly',
+    desc: 'Colorful fruity jelly cubes',
+  },
+];
